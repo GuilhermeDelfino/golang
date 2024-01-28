e6a5003fa16fc4a9e5bebf3ba204343c67d4a756
GuilhermeDelfino <guilherme.narciso@sptech.school>
2024-01-28 10:58:24 -0300

    aula1

---
+++ aula1/go.mod
@@ -0,0 +1,3 @@
+module delfino-aula1
+
+go 1.21.6
---
+++ aula1/main.go
@@ -0,0 +1,49 @@
+// CALCULADORA
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// SOMA, SUBTRACAO, MULTIPLICACAO, DIVISAO
+	// + , - , *, /
+	// 3 + 3 => 6
+	// 3-3 => 0
+	// 3*3 => 9
+
+	// Criar um scanner para ler do terminal
+	// scanner := bufio.NewScanner(os.Stdin)
+
+	// fmt.Print("Digite um numero: ")
+	// scanner.Scan()
+	// numero1 := scanner.Text()
+	// fmt.Print("Digite outro numero: ")
+	// scanner.Scan()
+	// numero2 := scanner.Text()
+
+	// numero1Inteiro, err := strconv.Atoi(numero1)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// numero2Inteiro, err := strconv.Atoi(numero2)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	if 2 == 3 {
+		fmt.Println("Verdade")
+	} else {
+		fmt.Println("Mentiraa")
+	}
+
+}
+
+// Funcao
+func soma(numero1 int, numero2 int) int {
+	return numero1 + numero2
+}
+
+func subtracao(numero1 int, numero2 int) *int {
+	subtracao := numero1 - numero2 // usando ponteiros
+	return &subtracao
+}
